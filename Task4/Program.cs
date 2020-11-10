0ce1426e97cf813d3d3cbf6d5007e4f0c4860c10
Даяна Тей <ditey@edu.hse.ru>
2020-11-10 19:19:34 +0300

    is editted

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -20,10 +20,32 @@ namespace Task4
 {
     class Program
     {
-        // TODO: самостоятельно выделите и напишите методы, использующиеся для решения задачи
-
         static void Main(string[] args)
         {
+            int n = GetNumber();
+            int m = GetNumber();
+
+            int result = Pow2(n) + Pow2(m);
+            Console.WriteLine(result);
+        }
+
+        static int GetNumber()
+        {
+            int number;
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out number) || number < 0)
+            {
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                Console.WriteLine("Ошибка"); Environment.Exit(0);
+            }
+
+            return number;
+        }
+
+        static int Pow2(int num)
+        {
+            return (int)Math.Pow(2, num);
         }
     }
 }
